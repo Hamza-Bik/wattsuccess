eb3d7d5067799e80acb3bd96018155e7761fec56
hamza-bik <hamza.bikourne@gmail.com>
2022-05-16 02:27:10 +0100

    home integration

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -9,6 +9,14 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+
+
+  goUp(){
+    document.getElementsByClassName('header')[0].scrollIntoView({ behavior: 'smooth' });
+    const btn = document.querySelector("#gotop");
+
+  }
+
   ngOnInit() {
   }
 
