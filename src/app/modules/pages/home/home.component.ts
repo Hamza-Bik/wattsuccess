528c2da4f26e02fac82a1990dea7925ecfcffa18
hamza-bik <hamza.bikourne@gmail.com>
2022-05-28 01:04:52 +0100

    add video popup

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as $ from 'jquery';
+import 'magnific-popup';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('img') imgElement: ElementRef | any;
 
   constructor() { }
-
-
 
   goUp(){
     document.getElementsByClassName('header')[0].scrollIntoView({ behavior: 'smooth' });
     const btn = document.querySelector("#gotop");
 
   }
-
+  ngAfterViewInit(): void {
+    (<any>$(this.imgElement.nativeElement)).magnificPopup({ type: 'iframe' });
+  }
   ngOnInit() {
   }
 
